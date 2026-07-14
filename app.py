4175d7678632504eba36415aada1aeb754ee537b
Gauri Sinha <gauriisinha7@gmail.com>
2026-07-14 23:43:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,3 +81,119 @@ def extract_skills(text):
 
 
     return found
+
+def calculate_skill_score(candidate_skills):
+
+    required = job_data["required_skills"]
+
+
+    matched = []
+
+
+    for skill in required:
+
+        if skill in candidate_skills:
+
+            matched.append(skill)
+
+
+    score = (
+
+        len(matched) / len(required)
+
+    ) * 100
+
+
+    return round(score,2), matched
+
+
+
+def calculate_experience_score(text):
+
+    matches = re.findall(
+
+        r'(\d+)\+?\s*(?:years|year|yrs)',
+
+        text.lower()
+
+    )
+
+
+    if matches:
+
+        years = max(
+
+            [int(x) for x in matches]
+
+        )
+
+    else:
+
+        years = 0
+
+
+
+    required_years = job_data["minimum_experience"]
+
+
+    if years >= required_years:
+
+        return 100
+
+    elif years > 0:
+
+        return 50
+
+    else:
+
+        return 0
+
+
+
+def calculate_education_score(text):
+
+    text = text.lower()
+
+
+    for edu in job_data["education"]:
+
+        if edu.lower() in text:
+
+            return 100
+
+
+    if "bachelor" in text:
+
+        return 100
+
+
+    return 0
+
+
+
+def calculate_final_score(
+
+    skill,
+
+    experience,
+
+    education
+
+):
+
+    final = (
+
+        skill * weights["Skills"]/100
+
+        +
+
+        experience * weights["Experience"]/100
+
+        +
+
+        education * weights["Education"]/100
+
+    )
+
+
+    return round(final,2)
