fc1b6aee6e02cc3366120d4b64c8378e3e75af7c
Gauri Sinha <gauriisinha7@gmail.com>
2026-07-15 00:07:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ st.set_page_config(
 st.title("AI-RDSS Candidate Recommendation System")
 
 
-# Load knowledge base
 
 with open("recruitment_knowledge_base.json","r") as f:
 
@@ -32,7 +31,53 @@ job_role = st.sidebar.selectbox(
 
 job_data = job_database[job_role]
 
+job_description_file = st.sidebar.file_uploader(
 
+    "Upload Job Description PDF",
+
+    type=["pdf"]
+
+)
+
+
+def extract_job_description(file):
+
+    text = ""
+
+    pdf = fitz.open(
+
+        stream=file.read(),
+
+        filetype="pdf"
+
+    )
+
+    for page in pdf:
+
+        text += page.get_text()
+
+
+    return text
+
+
+
+if job_description_file:
+
+    jd_text = extract_job_description(
+
+        job_description_file
+
+    )
+
+    st.sidebar.success(
+
+        "Job Description Loaded"
+
+    )
+
+else:
+
+    jd_text = ""
 weights = job_data["weights"]
 
 
